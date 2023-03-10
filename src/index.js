c364008c43a704db16afdb402c081d80796f6aef
Shan Kim <shankim35@gmail.com>
2023-03-10 17:25:24 -0500

    fix the canvas size

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import Player from "./scripts/player";
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 800;
+canvas.height = 600;
+context.fillRect(30, 30, canvas.width, canvas.height);
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const player = new Player(context, canvas);
   player.animate();
-})
+});
