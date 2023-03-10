79a30f305ed6e2810e221e9342066a2d2b37bacb
Shan Kim <shankim35@gmail.com>
2023-03-10 17:55:48 -0500

    tried building platform but doesn't appear on the canvas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Player from "./scripts/player";
-
+import Platform from "./scripts/terrain";
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
@@ -7,8 +7,9 @@ canvas.width = 800;
 canvas.height = 600;
 context.fillRect(30, 30, canvas.width, canvas.height);
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const player = new Player(context, canvas);
+  const platform = new Platform(context, canvas)
+  platform.draw();
   player.animate();
 });
--- src/scripts/terrain.js
+++ src/scripts/terrain.js
@@ -1,5 +1,27 @@
 class Platform {
-  constructor() {
-    
+
+  constructor(context, canvas) {
+    this.position = {
+      x: 300,
+      y: 500
+    }
+
+    this.width = 200
+    this.height = 20
+    this.context = context
+    this.canvas = canvas
   }
+
+  draw() {
+    this.context.fillStyle ='blue';
+    this.context.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+      );
+  }
+  
 }
+
+export default Platform;
